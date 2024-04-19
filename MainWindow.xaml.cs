14f8ba4e1e429d5e07715a2b3847f996a7a4b8dc
Zero <stalker_mr@list.ru>
2024-04-19 23:47:10 +0900

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowAuth.DataBase;
+
+namespace WindowAuth
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OdbConnectorHelper.odbobj = new AuthorDBEntities();
+
+            if (CheckboxRemember.IsChecked == true)
+            {
+                LoginTxbLoginTxb.Text = Properties.Settings.Default.SaveLogin;
+                PasswordBox.Password = Properties.Settings.Default.SavePassword;
+            }
+        }
+
+        private void Guest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы авторизовались как Гость");
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Переход на страницу Регистрации");
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = OdbConnectorHelper.odbobj.Users.FirstOrDefault(
+                    x => x.Login == LoginTxbLoginTxb.Text && x.Password == PasswordBox.Password
+                );
+                if (userObj == null)
+                {
+
+                    MessageBox.Show("Такой пользователь отсутствует в приложения",
+                        "Уведомление",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Warning);
+                }
+                else
+                {
+                    switch (userObj.Id)
+                    {
+                        case 1:
+                            MessageBox.Show("Вы вошли", "Informations", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        //case 2:
+                            
+                        //    break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Критический сбор в работе приложения", "Уведомление",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ UserControls/DownNavigates.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowAuth.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для DownNavigates.xaml
+    /// </summary>
+    public partial class DownNavigates : UserControl
+    {
+        public DownNavigates()
+        {
+            InitializeComponent();
+        }
+    }
+}
